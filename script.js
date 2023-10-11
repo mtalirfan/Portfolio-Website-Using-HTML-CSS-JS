264f3676ad0669d2a488b61e94816b5bc9c2cd46
mtalirfan <mtalirfan@gmail.com>
2023-10-11 20:52:06 +0500

    title icon metadata

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
-const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controls");
-const sectBtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-content");
+// const sections = document.querySelectorAll(".section");
+// const sectBtns = document.querySelectorAll(".controls");
+// const sectBtn = document.querySelectorAll(".control");
+// const allSections = document.querySelector(".main-content");
 
-function PageTransitions() {
-  // Button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
-      );
-      this.className += " active-btn";
-    });
-  }
-  // Section click active class
-  allSections.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      // remove selected from other btns
-      // sectBtns.forEach((btn) => {
-      //   btn.classList.remove("active");
-      // });
-      // // e.target.classList.add("active");
+// function PageTransitions() {
+//   // Button click active class
+//   for (let i = 0; i < sectBtn.length; i++) {
+//     sectBtn[i].addEventListener("click", function () {
+//       let currentBtn = document.querySelectorAll(".active-btn");
+//       currentBtn[0].className = currentBtn[0].className.replace(
+//         "active-btn",
+//         ""
+//       );
+//       this.className += " active-btn";
+//     });
+//   }
+//   // Section click active class
+//   allSections.addEventListener("click", (e) => {
+//     const id = e.target.dataset.id;
+//     if (id) {
+//       // remove selected from other btns
+//       // sectBtns.forEach((btn) => {
+//       //   btn.classList.remove("active");
+//       // });
+//       // // e.target.classList.add("active");
 
-      // hide other sections
-      sections.forEach((section) => {
-        section.classList.remove("active");
-      });
+//       // hide other sections
+//       sections.forEach((section) => {
+//         section.classList.remove("active");
+//       });
 
-      const element = document.getElementById(id);
-      element.classList.add("active");
-    }
-  });
-}
-
-PageTransitions();
-
-// function scroll() {
-//   document.addEventListener("scroll", function () {
-//     let target;
-//     if (window.scrollY >= window.scrollMaxY) {
-//       target = document.querySelector(".active-btn").nextElementSibling;
-//     } else if (window.scrollY <= 0) {
-//       target = document.querySelector(".active-btn").previousElementSibling;
+//       const element = document.getElementById(id);
+//       element.classList.add("active");
 //     }
-//     target && target.click();
 //   });
 // }
+// PageTransitions();
 
 function PageThemeTransitions() {
   [...document.querySelectorAll(".control")].forEach((button) => {
@@ -57,7 +44,6 @@ function PageThemeTransitions() {
       this.classList.add("active-btn");
       document.querySelector(".active").classList.remove("active");
       document.getElementById(button.dataset.id).classList.add("active");
-      // window.scrollTo(0, 1);
     });
   });
   document.querySelector(".theme-btn").addEventListener("click", () => {
@@ -66,4 +52,3 @@ function PageThemeTransitions() {
 }
 
 PageThemeTransitions();
-// scroll();
