ea8332dc3cbeaa8a925a9dfe8d895657c2e29282
mtalirfan <mtalirfan@gmail.com>
2023-10-19 21:43:26 +0500

    PWA cache image css js add

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,16 @@ self.addEventListener("install", (e) => {
     caches.open("static").then((cache) => {
       return cache.addAll([
         "./",
+        "./images/face.jpg",
+        "./images/coursera.jpg",
+        "./images/credly.jpg",
+        "./images/othercertificates.jpg",
         "./icons/android-chrome-192x192.png",
         "./icons/android-chrome-512x512.png",
+        "./styles/style.css",
+        "./404.html",
+        "./styles/404.css",
+        "./script.js",
       ]);
     })
   );
