00cfd966934fb3a5f16c15db5587ce779de8ab36
mtalirfan <mtalirfan@gmail.com>
2023-10-28 19:13:52 +0500

    save page state and styles refactor

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-window.addEventListener("load", function () {
+const loadStateFromStorage = () => {
   if (localStorage.getItem("theme") === "light") {
     themeClassToggle();
   } else {
     localStorage.setItem("theme", "dark");
   }
-});
+
+  let page = localStorage.getItem("page");
+  if (!page) {
+    localStorage.setItem("page", "home");
+    page = "home";
+  }
+  switch (page) {
+    case "home":
+      setPage(page);
+      break;
+    case "about":
+      setPage(page);
+      break;
+    case "portfolio":
+      setPage(page);
+      break;
+    case "blogs":
+      setPage(page);
+      break;
+    case "contact":
+      setPage(page);
+      break;
+    default:
+      page = "home";
+      setPage(page);
+      break;
+  }
+};
+
+const setPage = (page) => {
+  document.querySelector(".active-btn").classList.remove("active-btn");
+  document.querySelector(`[data-id="${page}"]`).classList.add("active-btn");
+  document.querySelector(".active").classList.remove("active");
+  document.getElementById(page).classList.add("active");
+  localStorage.setItem("page", page);
+};
 
 const pageTransitions = () => {
   [...document.querySelectorAll(".control")].forEach((button) => {
     button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
-      document.querySelector(".active").classList.remove("active");
-      document.getElementById(button.dataset.id).classList.add("active");
+      page = this.dataset.id;
+      setPage(page);
+      // document.querySelector(".active-btn").classList.remove("active-btn");
+      // this.classList.add("active-btn");
+      // document.querySelector(".active").classList.remove("active");
+      // document.getElementById(button.dataset.id).classList.add("active");
     });
   });
 };
@@ -40,8 +77,11 @@ const themeClassToggle = () => {
   });
 };
 
-pageTransitions();
-themeToggle();
+window.addEventListener("load", function () {
+  loadStateFromStorage();
+  pageTransitions();
+  themeToggle();
+});
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
