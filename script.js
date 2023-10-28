af67303e248f6b94662903e8296e0bfe718f9390
mtalirfan <mtalirfan@gmail.com>
2023-10-28 16:56:26 +0500

    local storage for theme and js cleanup

--- script.js
+++ script.js
@@ -1,52 +1,5 @@
-// const sections = document.querySelectorAll(".section");
-// const sectBtns = document.querySelectorAll(".controls");
-// const sectBtn = document.querySelectorAll(".control");
-// const allSections = document.querySelector(".main-content");
-
-// function PageTransitions() {
-//   // Button click active class
-//   for (let i = 0; i < sectBtn.length; i++) {
-//     sectBtn[i].addEventListener("click", function () {
-//       let currentBtn = document.querySelectorAll(".active-btn");
-//       currentBtn[0].className = currentBtn[0].className.replace(
-//         "active-btn",
-//         ""
-//       );
-//       this.className += " active-btn";
-//     });
-//   }
-//   // Section click active class
-//   allSections.addEventListener("click", (e) => {
-//     const id = e.target.dataset.id;
-//     if (id) {
-//       // remove selected from other btns
-//       // sectBtns.forEach((btn) => {
-//       //   btn.classList.remove("active");
-//       // });
-//       // // e.target.classList.add("active");
-
-//       // hide other sections
-//       sections.forEach((section) => {
-//         section.classList.remove("active");
-//       });
-
-//       const element = document.getElementById(id);
-//       element.classList.add("active");
-//     }
-//   });
-// }
-// PageTransitions();
-
-function PageThemeTransitions() {
-  [...document.querySelectorAll(".control")].forEach((button) => {
-    button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
-      document.querySelector(".active").classList.remove("active");
-      document.getElementById(button.dataset.id).classList.add("active");
-    });
-  });
-  document.querySelector(".theme-btn").addEventListener("click", () => {
+window.addEventListener("load", function () {
+  if (localStorage.getItem("theme") === "light") {
     document.body.classList.toggle("light-mode");
     document.querySelector(".theme-btn").classList.toggle("light-mode");
     document.querySelectorAll(".section-shape").forEach((shape) => {
@@ -55,10 +8,43 @@ function PageThemeTransitions() {
     document.querySelectorAll(".shape-text").forEach((shapeText) => {
       shapeText.classList.toggle("light-mode");
     });
-  });
-}
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+});
 
-PageThemeTransitions();
+const pageTransitions = () => {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+};
+
+const themeToggle = () => {
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (localStorage.getItem("theme") === "dark") {
+      localStorage.setItem("theme", "light");
+    } else if (localStorage.getItem("theme") === "light") {
+      localStorage.setItem("theme", "dark");
+    }
+    document.querySelector(".theme-btn").classList.toggle("light-mode");
+    document.querySelectorAll(".section-shape").forEach((shape) => {
+      shape.classList.toggle("light-mode");
+    });
+    document.querySelectorAll(".shape-text").forEach((shapeText) => {
+      shapeText.classList.toggle("light-mode");
+    });
+  });
+};
+
+pageTransitions();
+themeToggle();
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
